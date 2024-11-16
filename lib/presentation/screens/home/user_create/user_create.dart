6c5a9ee42e30a12065654be0f452aa4fc66a33e5
DavidCa25 <banerdavid29@gmail.com>
2024-11-16 11:08:17 -0600

    Inicialicé proyecto

---
+++ lib/presentation/screens/home/user_create/user_create.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class UserCreate extends StatelessWidget {
+  const UserCreate({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
---
+++ lib/presentation/widgets/user_textfield.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class UserTextfield extends StatelessWidget {
+  const UserTextfield({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
